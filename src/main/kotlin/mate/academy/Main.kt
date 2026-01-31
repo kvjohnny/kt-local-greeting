35793acde7f9edf0913a5144a491ca0fb6a84641
kvjohnny <kvjohnny.van@gmail.com>
2026-01-31 18:09:07 +0200

    second commit in kt-local-greeting task

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,15 +1,14 @@
 package mate.academy
 
-fun greetUsers() {
-    var name: String
-    while (true) {
-        println("Please enter a name (or press Enter to quit):")
-        name = readln()
-        if (name.isEmpty()) break
-        println("Hello, $name!")
-    }
-}
-
 fun main() {
+    fun greetUsers() {
+        var name: String
+        while (true) {
+            println("Please enter a name (or press Enter to quit):")
+            name = readln()
+            if (name.isEmpty()) break
+            println("Hello, $name!")
+        }
+    }
     greetUsers()
 }
