75ca0625c9aaedbabdb5df6b67860acdcd9aa015
kvjohnny <kvjohnny.van@gmail.com>
2026-01-31 18:04:09 +0200

    fixed file in kt-local-greeting task

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,13 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    var name: String
+    while (true) {
+        println("Please enter a name (or press Enter to quit):")
+        name = readln()
+        if (name.isEmpty()) break
+        println("Hello, $name!")
+    }
 }
 
 fun main() {
